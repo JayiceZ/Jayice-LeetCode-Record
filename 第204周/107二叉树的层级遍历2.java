0c5f8685338f4113530ce77fd7031b991fee4322
JAYICE <49588871+1185430411@users.noreply.github.com>
2020-08-27 12:43:45 +0800

    Create 107二叉树的层级遍历2.java

---
+++ 第204周/107二叉树的层级遍历2.java
@@ -0,0 +1,56 @@
+给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其自底向上的层次遍历为：
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> ans=new LinkedList();
+        if(root==null)
+            return ans;
+        Queue<TreeNode>queue=new LinkedList();
+        queue.add(root);
+        while(queue.size()!=0){
+            int size=queue.size();
+            List<Integer>list=new ArrayList();
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                list.add(node.val);
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            ans.addFirst(list);
+        }
+        return ans;
+    }
+}
