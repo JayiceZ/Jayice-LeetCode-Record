7c816e5e78e97d0b2dd46f622f5742383f27213f
JAYICE <49588871+1185430411@users.noreply.github.com>
2020-08-29 15:23:57 +0800

    Create 543二叉树直径.java

---
+++ 第204周/543二叉树直径.java
@@ -0,0 +1,47 @@
+543. 二叉树的直径
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+
+ 
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+ 
+
+注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+    private int helper(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int left=helper(root.left);
+        int right=helper(root.right);
+
+        ans=Math.max(ans,left+right);
+        
+        return Math.max(left,right)+1;
+    }
+}
