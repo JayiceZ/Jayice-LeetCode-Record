5173735e423c1bc559a8e97bd4f95f1cc9e9c860
JAYICE <49588871+Jayice-zjw@users.noreply.github.com>
2020-09-16 23:24:04 +0800

    Create 21. 合并两个有序链表.java

---
+++ 第207周/21. 合并两个有序链表.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ans=new ListNode(-1);
+        ListNode temp=ans;
+        while(l1!=null&&l2!=null){
+            if(l1.val<l2.val){
+                temp.next=l1;
+                l1=l1.next;
+            }else{
+                temp.next=l2;
+                l2=l2.next;
+            }
+            temp=temp.next;
+        }
+        temp.next=l1==null?l2:l1;
+        return ans.next;
+    }
+}
