54d2a9643c84d3c9e4fd2408a627b954007ead16
JAYICE <49588871+Jayice-zjw@users.noreply.github.com>
2020-09-17 23:30:44 +0800

    Update 1. 两数之和.java

--- 第207周/1. 两数之和.java
+++ 第207周/1. 两数之和.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer>map=new HashMap();
+        Map<Integer,Integer>map=new HashMap(); 
         for(int i=0;i<nums.length;i++){
             int num=nums[i];
             if(map.containsKey(target-num)){
