c798ccb02891f1869a60bde98348ffca5d3e4a37
JAYICE <49588871+Jayice-zjw@users.noreply.github.com>
2020-09-22 21:34:05 +0800

    Create 96. 不同的二叉搜索树.java

---
+++ 第208周/96. 不同的二叉搜索树.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numTrees(int n) {
+        int[]dp=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i/2;j++){
+                dp[i]+=2*dp[j]*dp[i-1-j];
+            }
+            if(i%2==1)
+                dp[i]+=(dp[i/2]*dp[i/2]);
+        }
+        return dp[n];
+    }
+}
